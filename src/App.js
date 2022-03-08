80b04c3fc76fc2b5178fa959fd0ac2244e4dd05a
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-09 10:53:59 +1100

    Update App.js
    Change fetch to get new characters at the click of the button

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ export default function App() {
   const [starWarsData, setStarWarsData] = useState({});
   const [count, setCount] = useState(0);
 
-  console.log("Component rendered");
-
   useEffect(() => {
-    fetch("https://swapi.dev/api/people/1")
+    console.log("Effect ran");
+    fetch(`https://swapi.dev/api/people/${count}`)
       .then((response) => response.json())
-    .then((data) => setStarWarsData(data));
+      .then((data) => setStarWarsData(data));
   }, [count]);
 
   return (
     <div>
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
       <h2>The count is {count}</h2>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        Add
+        Get Next Character
       </button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
 }
